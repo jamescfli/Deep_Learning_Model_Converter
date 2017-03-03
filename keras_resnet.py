6056b9796eeb389892054520ca399ae11f5f2619
jamescfli <jamescfli@yahoo.com>
2017-03-03 13:46:18 +0800

    visualize resnet50 net with/without top

---
+++ keras_resnet.py
@@ -0,0 +1,8 @@
+from keras.applications import resnet50, vgg16
+from keras.utils.visualize_util import plot
+
+resnet50_keras = resnet50.ResNet50(include_top=True, weights='imagenet', input_shape=(224, 224, 3))
+resnet50_keras.summary()
+
+# plot(resnet50_keras, './Keras/visualized/resnet50_notop_keras_graph.png', show_shapes=True, show_layer_names=True)
+plot(resnet50_keras, './Keras/visualized/resnet50_wtop_keras_graph.png', show_shapes=True, show_layer_names=True)
